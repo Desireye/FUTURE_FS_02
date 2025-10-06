5ea93b5d66b4ffd3c5b34bd435b66f3888baf6dd
Modesire Soneye <modesiresoneye@gmail.com>
2025-10-06 16:17:35 +0100

    error fixes

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://future-fs-02-r5cj.onrender.com/"
+    ? "https://future-fs-02-r5cj.onrender.com"
     : "http://localhost:3000";
 
 export default API_BASE_URL;
